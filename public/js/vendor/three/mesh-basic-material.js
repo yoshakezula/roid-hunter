9c02f65a0c20933b63b2d20b4d36644ae8face90
Jason Pearson <jason.d.pearson@gmail.com>
2013-04-21 02:33:52 -0400

    more libs

---
+++ public/js/vendor/three/mesh-basic-material.js
@@ -0,0 +1,111 @@
+define(['utilities/window','three','color','material'],function(window) {
+	/**
+	 * @author mrdoob / http://mrdoob.com/
+	 * @author alteredq / http://alteredqualia.com/
+	 *
+	 * parameters = {
+	 *  color: <hex>,
+	 *  opacity: <float>,
+	 *  map: new THREE.Texture( <Image> ),
+	 *
+	 *  lightMap: new THREE.Texture( <Image> ),
+	 *
+	 *  specularMap: new THREE.Texture( <Image> ),
+	 *
+	 *  envMap: new THREE.TextureCube( [posx, negx, posy, negy, posz, negz] ),
+	 *  combine: THREE.Multiply,
+	 *  reflectivity: <float>,
+	 *  refractionRatio: <float>,
+	 *
+	 *  shading: THREE.SmoothShading,
+	 *  blending: THREE.NormalBlending,
+	 *  depthTest: <bool>,
+	 *  depthWrite: <bool>,
+	 *
+	 *  wireframe: <boolean>,
+	 *  wireframeLinewidth: <float>,
+	 *
+	 *  vertexColors: THREE.NoColors / THREE.VertexColors / THREE.FaceColors,
+	 *
+	 *  skinning: <bool>,
+	 *  morphTargets: <bool>,
+	 *
+	 *  fog: <bool>
+	 * }
+	 */
+
+	window.THREE.MeshBasicMaterial = function ( parameters ) {
+
+		THREE.Material.call( this );
+
+		this.color = new THREE.Color( 0xffffff ); // emissive
+
+		this.map = null;
+
+		this.lightMap = null;
+
+		this.specularMap = null;
+
+		this.envMap = null;
+		this.combine = THREE.MultiplyOperation;
+		this.reflectivity = 1;
+		this.refractionRatio = 0.98;
+
+		this.fog = true;
+
+		this.shading = THREE.SmoothShading;
+
+		this.wireframe = false;
+		this.wireframeLinewidth = 1;
+		this.wireframeLinecap = 'round';
+		this.wireframeLinejoin = 'round';
+
+		this.vertexColors = THREE.NoColors;
+
+		this.skinning = false;
+		this.morphTargets = false;
+
+		this.setValues( parameters );
+
+	};
+
+	window.THREE.MeshBasicMaterial.prototype = Object.create( THREE.Material.prototype );
+
+	window.THREE.MeshBasicMaterial.prototype.clone = function () {
+
+		var material = new THREE.MeshBasicMaterial();
+
+		THREE.Material.prototype.clone.call( this, material );
+
+		material.color.copy( this.color );
+
+		material.map = this.map;
+
+		material.lightMap = this.lightMap;
+
+		material.specularMap = this.specularMap;
+
+		material.envMap = this.envMap;
+		material.combine = this.combine;
+		material.reflectivity = this.reflectivity;
+		material.refractionRatio = this.refractionRatio;
+
+		material.fog = this.fog;
+
+		material.shading = this.shading;
+
+		material.wireframe = this.wireframe;
+		material.wireframeLinewidth = this.wireframeLinewidth;
+		material.wireframeLinecap = this.wireframeLinecap;
+		material.wireframeLinejoin = this.wireframeLinejoin;
+
+		material.vertexColors = this.vertexColors;
+
+		material.skinning = this.skinning;
+		material.morphTargets = this.morphTargets;
+
+		return material;
+
+	};
+
+});
